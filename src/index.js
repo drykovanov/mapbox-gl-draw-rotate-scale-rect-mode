c00ce16de97ab040ae75a803d71f6550fd316674
drykovanov <drykovanov@gmail.com>
2019-11-18 14:49:00 +0300

    Test/demo page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,316 @@
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
+import * as turf from '@turf/turf';
 
 export const TxRectMode = {};
 
 
+function tx_rect_mode_demo_map_onload(event) {
 
+    var map = event.target;
+
+    var draw = new MapboxDraw({
+        displayControlsDefault: true,
+        // styles: drawStyles,
+        modes: Object.assign({
+            draw_rect: TxRectMode,
+        }, MapboxDraw.modes),
+
+        styles: [
+            {
+                'id': 'gl-draw-polygon-fill-inactive',
+                'type': 'fill',
+                'filter': ['all',
+                    ['==', 'active', 'false'],
+                    ['==', '$type', 'Polygon'],
+                    ['!=', 'mode', 'static']
+                ],
+                'paint': {
+                    'fill-color': '#3bb2d0',
+                    'fill-outline-color': '#3bb2d0',
+                    'fill-opacity': 0.0
+                }
+            },
+            {
+                'id': 'gl-draw-polygon-fill-active',
+                'type': 'fill',
+                'filter': ['all', ['==', 'active', 'true'], ['==', '$type', 'Polygon']],
+                'paint': {
+                    'fill-color': '#fbb03b',
+                    'fill-outline-color': '#fbb03b',
+                    'fill-opacity': 0.0
+                }
+            },
+            {
+                'id': 'gl-draw-polygon-midpoint',
+                'type': 'circle',
+                'filter': ['all',
+                    ['==', '$type', 'Point'],
+                    ['==', 'meta', 'midpoint']],
+                'paint': {
+                    'circle-radius': 3,
+                    'circle-color': '#fbb03b'
+                }
+            },
+            {
+                'id': 'gl-draw-polygon-stroke-inactive',
+                'type': 'line',
+                'filter': ['all',
+                    ['==', 'active', 'false'],
+                    ['==', '$type', 'Polygon'],
+                    ['!=', 'mode', 'static']
+                ],
+                'layout': {
+                    'line-cap': 'round',
+                    'line-join': 'round'
+                },
+                'paint': {
+                    'line-color': '#3bb2d0',
+                    'line-width': 2
+                }
+            },
+            {
+                'id': 'gl-draw-polygon-stroke-active',
+                'type': 'line',
+                'filter': ['all', ['==', 'active', 'true'], ['==', '$type', 'Polygon']],
+                'layout': {
+                    'line-cap': 'round',
+                    'line-join': 'round'
+                },
+                'paint': {
+                    'line-color': '#fbb03b',
+                    'line-dasharray': [0.2, 2],
+                    'line-width': 2
+                }
+            },
+            {
+                'id': 'gl-draw-line-inactive',
+                'type': 'line',
+                'filter': ['all',
+                    ['==', 'active', 'false'],
+                    ['==', '$type', 'LineString'],
+                    ['!=', 'mode', 'static']
+                ],
+                'layout': {
+                    'line-cap': 'round',
+                    'line-join': 'round'
+                },
+                'paint': {
+                    'line-color': '#3bb2d0',
+                    'line-width': 2
+                }
+            },
+            {
+                'id': 'gl-draw-line-active',
+                'type': 'line',
+                'filter': ['all',
+                    ['==', '$type', 'LineString'],
+                    ['==', 'active', 'true']
+                ],
+                'layout': {
+                    'line-cap': 'round',
+                    'line-join': 'round'
+                },
+                'paint': {
+                    'line-color': '#fbb03b',
+                    'line-dasharray': [0.2, 2],
+                    'line-width': 2
+                }
+            },
+            {
+                'id': 'gl-draw-polygon-and-line-vertex-stroke-inactive',
+                'type': 'circle',
+                'filter': ['all',
+                    ['==', 'meta', 'vertex'],
+                    ['==', '$type', 'Point'],
+                    ['!=', 'mode', 'static']
+                ],
+                'paint': {
+                    'circle-radius': 5,
+                    'circle-color': '#fff'
+                }
+            },
+            {
+                'id': 'gl-draw-polygon-and-line-vertex-inactive',
+                'type': 'circle',
+                'filter': ['all',
+                    ['==', 'meta', 'vertex'],
+                    ['==', '$type', 'Point'],
+                    ['!=', 'mode', 'static']
+                ],
+                'paint': {
+                    'circle-radius': 3,
+                    'circle-color': '#fbb03b'
+                }
+            },
+            {
+                'id': 'gl-draw-point-point-stroke-inactive',
+                'type': 'circle',
+                'filter': ['all',
+                    ['==', 'active', 'false'],
+                    ['==', '$type', 'Point'],
+                    ['==', 'meta', 'feature'],
+                    ['!=', 'mode', 'static']
+                ],
+                'paint': {
+                    'circle-radius': 5,
+                    'circle-opacity': 1,
+                    'circle-color': '#fff'
+                }
+            },
+            {
+                'id': 'gl-draw-point-inactive',
+                'type': 'circle',
+                'filter': ['all',
+                    ['==', 'active', 'false'],
+                    ['==', '$type', 'Point'],
+                    ['==', 'meta', 'feature'],
+                    ['!=', 'mode', 'static']
+                ],
+                'paint': {
+                    'circle-radius': 3,
+                    'circle-color': '#3bb2d0'
+                }
+            },
+            {
+                'id': 'gl-draw-point-stroke-active',
+                'type': 'circle',
+                'filter': ['all',
+                    ['==', '$type', 'Point'],
+                    ['==', 'active', 'true'],
+                    ['!=', 'meta', 'midpoint']
+                ],
+                'paint': {
+                    'circle-radius': 7,
+                    'circle-color': '#fff'
+                }
+            },
+            {
+                'id': 'gl-draw-point-active',
+                'type': 'circle',
+                'filter': ['all',
+                    ['==', '$type', 'Point'],
+                    ['!=', 'meta', 'midpoint'],
+                    ['==', 'active', 'true']],
+                'paint': {
+                    'circle-radius': 5,
+                    'circle-color': '#fbb03b'
+                }
+            },
+            {
+                'id': 'gl-draw-polygon-fill-static',
+                'type': 'fill',
+                'filter': ['all', ['==', 'mode', 'static'], ['==', '$type', 'Polygon']],
+                'paint': {
+                    'fill-color': '#404040',
+                    'fill-outline-color': '#404040',
+                    'fill-opacity': 0.1
+                }
+            },
+            {
+                'id': 'gl-draw-polygon-stroke-static',
+                'type': 'line',
+                'filter': ['all', ['==', 'mode', 'static'], ['==', '$type', 'Polygon']],
+                'layout': {
+                    'line-cap': 'round',
+                    'line-join': 'round'
+                },
+                'paint': {
+                    'line-color': '#404040',
+                    'line-width': 2
+                }
+            },
+            {
+                'id': 'gl-draw-line-static',
+                'type': 'line',
+                'filter': ['all', ['==', 'mode', 'static'], ['==', '$type', 'LineString']],
+                'layout': {
+                    'line-cap': 'round',
+                    'line-join': 'round'
+                },
+                'paint': {
+                    'line-color': '#404040',
+                    'line-width': 2
+                }
+            },
+            {
+                'id': 'gl-draw-point-static',
+                'type': 'circle',
+                'filter': ['all', ['==', 'mode', 'static'], ['==', '$type', 'Point']],
+                'paint': {
+                    'circle-radius': 5,
+                    'circle-color': '#404040'
+                }
+            }
+        ]
+    });
+
+
+
+    // nyc_1911.jpg - 468x760
+
+    var im_w = 468;
+    var im_h = 760;
+
+    const canvas = map.getCanvas();
+    // Get the device pixel ratio, falling back to 1.
+    var dpr = window.devicePixelRatio || 1;
+    // Get the size of the canvas in CSS pixels.
+    var rect = canvas.getBoundingClientRect();
+    const w = rect.width;
+    const h = rect.height;
+    console.log('canvas: ' + w + 'x' + h);
+
+    while (im_w >= w || im_h >= h) {
+        im_w = Math.round(0.8 * im_w);
+        im_h = Math.round(0.8 * im_h);
+    }
+
+    const cUL = map.unproject ([w/2 - im_w/2, h/2 - im_h/2]).toArray();
+    const cUR = map.unproject ([w/2 + im_w/2, h/2 - im_h/2]).toArray();
+    const cLR = map.unproject ([w/2 + im_w/2, h/2 + im_h/2]).toArray();
+    const cLL = map.unproject ([w/2 - im_w/2, h/2 + im_h/2]).toArray();
+    const coordinates = [cUL,cUR,cLR,cLL,cUL];
+    const polygon = turf.polygon([coordinates]);
+    polygon.id = 1;
+
+
+
+    map.addSource("test-overlay", {
+        "type": "image",
+        "url": 'nyc_1911.jpg',
+        "coordinates": [cUL,cUR,cLR,cLL]
+    });
+
+    map.addLayer({
+        "id": "test-overlay-layer",
+        "type": "raster",
+        "source": "test-overlay",
+        "paint": {"raster-opacity": 0.90},
+    });
+
+
+
+    map.addControl(draw, 'top-right');
+    map.on('draw.update', drawUpdateOverlay.bind({
+        map: map
+    }));
+
+
+    draw.add(polygon);
+    // draw.changeMode('mode', opts);
+    draw.changeMode('direct_select', {
+        featureId: polygon.id
+    });
+}
+
+function drawUpdateOverlay(e) {
+    var feature = e.features[0];
+    var coordinates = feature.geometry.coordinates[0].slice(0, 4);
+
+    console.log(coordinates);
+
+    this.map.getSource("test-overlay").setCoordinates(coordinates);
+}
 
 export function tx_rect_mode_demo() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiZHJ5a292YW5vdiIsImEiOiJjazM0OG9hYW4wenR4M2xtajVseW1qYjY3In0.YnbkeuaBiSaDOn7eYDAXsQ';
@@ -13,4 +320,10 @@ export function tx_rect_mode_demo() {
         center: [-73.93, 40.73], // starting position [lng, lat]
         zoom: 10 // starting zoom
     });
+
+    map.on('load', tx_rect_mode_demo_map_onload);
+
+
+
+
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
 
+    devtool: "source-map",
+    mode: 'development',
+
     entry: [
         './src/index.js'
     ],
