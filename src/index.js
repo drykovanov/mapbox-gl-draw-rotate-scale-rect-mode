d7c7923154f150c5096a7dc736bf3eaa9bedb2af
drykovanov <drykovanov@gmail.com>
2019-11-21 16:28:01 +0300

    rotation and scale icons, rotated

--- src/index.js
+++ src/index.js
@@ -19,8 +19,10 @@ TxRectMode.toDisplayFeatures = function(state, geojson, push) {
             midpoints: false,
             selectedPaths: state.selectedCoordPaths
         });
+        this.computeBisectrix(suppPoints);
+        var rotPoints = this.createRotationPoints(geojson, suppPoints);
         suppPoints.forEach(push);
-        this.createRotationPoint(geojson, suppPoints[0], suppPoints[1]).forEach(push);
+        rotPoints.forEach(push);
     } else {
         geojson.properties.active = Constants.activeStates.INACTIVE;
         push(geojson);
@@ -82,55 +84,94 @@ TxRectMode.pathsToCoordinates = function(featureId, paths) {
     return paths.map(coord_path => { return { feature_id: featureId, coord_path }; });
 };
 
-TxRectMode.createRotationPoint = function(geojson, v0, v1) {
+TxRectMode.computeBisectrix = function(points) {
+    console.log('computeBisectrix()');
+
+
+    for (var i1 = 0; i1 < points.length; i1++) {
+        var i0 = (i1 - 1 + points.length) % points.length;
+        var i2 = (i1 + 1) % points.length;
+        // console.log('' + i0 + ' -> ' + i1 + ' -> ' + i2);
+
+        var l1 = turf.lineString([points[i0].geometry.coordinates, points[i1].geometry.coordinates]);
+        var l2 = turf.lineString([points[i1].geometry.coordinates, points[i2].geometry.coordinates]);
+        var a1 = turf.bearing(points[i0].geometry.coordinates, points[i1].geometry.coordinates);
+        var a2 = turf.bearing(points[i2].geometry.coordinates, points[i1].geometry.coordinates);
+        // console.log('a1 = '  +a1 + ', a2 = ' + a2);
+
+        var a = (a1 + a2)/2.0;
+
+        if (a < 0.0)
+            a += 360;
+        if (a > 360)
+            a -= 360;
+
+        points[i1].properties.heading = a;
+    }
+
+};
+
+TxRectMode.createRotationPoints = function(geojson, suppPoints) {
     const { type, coordinates } = geojson.geometry;
     const featureId = geojson.properties && geojson.properties.id;
 
     let rotationWidgets = [];
-    if (type === Constants.geojsonTypes.POLYGON && v0 && v1) {
-        var center = turf.centroid(geojson);
-        var cR0 = turf.midpoint(v0, v1).geometry.coordinates;
-
-        var heading = turf.bearing(center, cR0);
-        var distance0 = turf.distance(center, cR0);
-        var distance1 = 1.1 * distance0; // TODO depends on map scale
-        var cR1 = turf.destination(center, distance1, heading, {}).geometry.coordinates;
-
-        rotationWidgets.push({
-                type: Constants.geojsonTypes.FEATURE,
-                properties: {
-                    meta: Constants.meta.MIDPOINT,
-                    parent: featureId,
-                    lng: cR1[0],
-                    lat: cR1[1],
-                    coord_path: v1.properties.coord_path
-                },
-                geometry: {
-                    type: Constants.geojsonTypes.POINT,
-                    coordinates: cR1
-                }
-            }
-        );
-
-        rotationWidgets.push({
-                type: Constants.geojsonTypes.FEATURE,
-                properties: {
-                    meta: Constants.meta.MIDPOINT,
-                    parent: featureId,
-                    // lng: cR1[0],
-                    // lat: cR1[1],
-                    coord_path: v1.properties.coord_path
-                },
-                geometry: {
-                    type: Constants.geojsonTypes.LINE_STRING,
-                    coordinates: [cR0, cR1]
-                }
-            }
-        );
-
-
-
+    if (type != Constants.geojsonTypes.POLYGON) {
+        return ;
     }
+
+    var corners = suppPoints.slice(0);
+    corners[corners.length] = corners[0];
+
+    var v1 = null;
+    corners.forEach((v2) => {
+        if (v1 != null) {
+            var center = turf.centroid(geojson);
+            var cR0 = turf.midpoint(v1, v2).geometry.coordinates;
+
+            var heading = turf.bearing(center, cR0);
+            var distance0 = turf.distance(center, cR0);
+            var distance1 = 1.0 * distance0; // TODO paramter, TODO depends on map scale
+            var cR1 = turf.destination(center, distance0, heading, {}).geometry.coordinates;
+
+            rotationWidgets.push({
+                    type: Constants.geojsonTypes.FEATURE,
+                    properties: {
+                        meta: Constants.meta.MIDPOINT,
+                        parent: featureId,
+                        lng: cR1[0],
+                        lat: cR1[1],
+                        coord_path: v1.properties.coord_path,
+                        heading: heading,
+                    },
+                    geometry: {
+                        type: Constants.geojsonTypes.POINT,
+                        coordinates: cR1
+                    }
+                }
+            );
+
+            // rotationWidgets.push({
+            //         type: Constants.geojsonTypes.FEATURE,
+            //         properties: {
+            //             meta: Constants.meta.MIDPOINT,
+            //             parent: featureId,
+            //             // lng: cR1[0],
+            //             // lat: cR1[1],
+            //             coord_path: v1.properties.coord_path
+            //         },
+            //         geometry: {
+            //             type: Constants.geojsonTypes.LINE_STRING,
+            //             coordinates: [cR0, cR1]
+            //         }
+            //     }
+            // );
+        }
+
+        v1 = v2;
+
+    });
+
     return rotationWidgets;
 };
 
@@ -472,7 +513,7 @@ var drawStyle = [
             ['!=', 'mode', 'static']
         ],
         'paint': {
-            'circle-radius': 10,
+            'circle-radius': 4,
             'circle-color': '#fff'
         }
     },
@@ -485,10 +526,35 @@ var drawStyle = [
             ['!=', 'mode', 'static']
         ],
         'paint': {
-            'circle-radius': 6,
+            'circle-radius': 2,
             'circle-color': '#fbb03b'
         }
     },
+
+    {
+        'id': 'gl-draw-polygon-and-line-vertex-scale-icon',
+        'type': 'symbol',
+        'filter': ['all',
+            ['==', 'meta', 'vertex'],
+            ['==', '$type', 'Point'],
+            ['!=', 'mode', 'static']
+        ],
+        'layout': {
+            'icon-image': 'scale',
+            'icon-allow-overlap': true,
+            'icon-ignore-placement': true,
+            'icon-rotation-alignment': 'map',
+            'icon-rotate': ['get', 'heading']
+        },
+        'paint': {
+            'icon-opacity': 1.0,
+            'icon-opacity-transition': {
+                'delay': 0,
+                'duration': 0
+            }
+        }
+    },
+
 
     {
         'id': 'gl-draw-point-point-stroke-inactive',
@@ -528,7 +594,7 @@ var drawStyle = [
             ['!=', 'meta', 'midpoint']
         ],
         'paint': {
-            'circle-radius': 7,
+            'circle-radius': 4,
             'circle-color': '#fff'
         }
     },
@@ -540,7 +606,7 @@ var drawStyle = [
             ['!=', 'meta', 'midpoint'],
             ['==', 'active', 'true']],
         'paint': {
-            'circle-radius': 5,
+            'circle-radius': 2,
             'circle-color': '#fbb03b'
         }
     },
@@ -631,7 +697,7 @@ var drawStyle = [
             ['!=', 'mode', 'static']
         ],
         'paint': {
-            'circle-radius': 15,
+            'circle-radius': 4,
             'circle-color': '#fff'
         }
     },
@@ -644,7 +710,7 @@ var drawStyle = [
             ['!=', 'mode', 'static']
         ],
         'paint': {
-            'circle-radius': 12,
+            'circle-radius': 2,
             'circle-color': '#fbb03b'
         }
     },
@@ -659,7 +725,9 @@ var drawStyle = [
         'layout': {
             'icon-image': 'rotate',
             'icon-allow-overlap': true,
-            'icon-ignore-placement': true
+            'icon-ignore-placement': true,
+            'icon-rotation-alignment': 'map',
+            'icon-rotate': ['get', 'heading']
         },
         'paint': {
             'icon-opacity': 1.0,
@@ -675,78 +743,82 @@ function tx_rect_mode_demo_map_onload(event) {
 
     var map = event.target;
 
-    map.loadImage('rotation-3d-24x24.png', function(error, image) {
+    map.loadImage('rotate/01.png', function(error, image) {
         if (error) throw error;
-        // Add the loaded image to the style's sprite with the ID 'kitten'.
         map.addImage('rotate', image);
-
-        var draw = new MapboxDraw({
-            displayControlsDefault: true,
-            // styles: drawStyles,
-            modes: Object.assign({
-                tx_rect: TxRectMode,
-            }, MapboxDraw.modes),
-
-            styles: drawStyle,
-        });
-
-        // nyc_1911.jpg - 468x760
-
-        var im_w = 421;
-        var im_h = 671;
-
-        const canvas = map.getCanvas();
-        // Get the device pixel ratio, falling back to 1.
-        var dpr = window.devicePixelRatio || 1;
-        // Get the size of the canvas in CSS pixels.
-        var rect = canvas.getBoundingClientRect();
-        const w = rect.width;
-        const h = rect.height;
-        // console.log('canvas: ' + w + 'x' + h);
-
-        while (im_w >= (0.8 * w) || im_h >= (0.8 * h)) {
-            im_w = Math.round(0.8 * im_w);
-            im_h = Math.round(0.8 * im_h);
-        }
-
-        const cUL = map.unproject ([w/2 - im_w/2, h/2 - im_h/2]).toArray();
-        const cUR = map.unproject ([w/2 + im_w/2, h/2 - im_h/2]).toArray();
-        const cLR = map.unproject ([w/2 + im_w/2, h/2 + im_h/2]).toArray();
-        const cLL = map.unproject ([w/2 - im_w/2, h/2 + im_h/2]).toArray();
-        const coordinates = [cUL,cUR,cLR,cLL,cUL];
-        const polygon = turf.polygon([coordinates]);
-        polygon.id = 1;
-
-        map.addSource("test-overlay", {
-            "type": "image",
-            "url": 'nyc_1911_crop.jpg',
-            "coordinates": [cUL,cUR,cLR,cLL]
-        });
-
-        map.addLayer({
-            "id": "test-overlay-layer",
-            "type": "raster",
-            "source": "test-overlay",
-            "paint": {
-                "raster-opacity": 0.90,
-                "raster-fade-duration": 0
-            },
-        });
-
-        map.addControl(draw, 'top-right');
-        map.on('draw.update', drawUpdateOverlay.bind({
-            map: map
-        }));
-
-
-        draw.add(polygon);
-        // draw.changeMode('mode', opts);
-        // tx_rect, direct_select
-        draw.changeMode('tx_rect', {
-            featureId: polygon.id
-        });
-
     });
+
+    map.loadImage('scale/01.png', function(error, image) {
+        if (error) throw error;
+        map.addImage('scale', image);
+    });
+
+    var draw = new MapboxDraw({
+        displayControlsDefault: true,
+        // styles: drawStyles,
+        modes: Object.assign({
+            tx_rect: TxRectMode,
+        }, MapboxDraw.modes),
+
+        styles: drawStyle,
+    });
+
+    // nyc_1911.jpg - 468x760
+
+    var im_w = 421;
+    var im_h = 671;
+
+    const canvas = map.getCanvas();
+    // Get the device pixel ratio, falling back to 1.
+    var dpr = window.devicePixelRatio || 1;
+    // Get the size of the canvas in CSS pixels.
+    var rect = canvas.getBoundingClientRect();
+    const w = rect.width;
+    const h = rect.height;
+    // console.log('canvas: ' + w + 'x' + h);
+
+    while (im_w >= (0.8 * w) || im_h >= (0.8 * h)) {
+        im_w = Math.round(0.8 * im_w);
+        im_h = Math.round(0.8 * im_h);
+    }
+
+    const cUL = map.unproject ([w/2 - im_w/2, h/2 - im_h/2]).toArray();
+    const cUR = map.unproject ([w/2 + im_w/2, h/2 - im_h/2]).toArray();
+    const cLR = map.unproject ([w/2 + im_w/2, h/2 + im_h/2]).toArray();
+    const cLL = map.unproject ([w/2 - im_w/2, h/2 + im_h/2]).toArray();
+    const coordinates = [cUL,cUR,cLR,cLL,cUL];
+    const polygon = turf.polygon([coordinates]);
+    polygon.id = 1;
+
+    map.addSource("test-overlay", {
+        "type": "image",
+        "url": 'nyc_1911_crop.jpg',
+        "coordinates": [cUL,cUR,cLR,cLL]
+    });
+
+    map.addLayer({
+        "id": "test-overlay-layer",
+        "type": "raster",
+        "source": "test-overlay",
+        "paint": {
+            "raster-opacity": 0.90,
+            "raster-fade-duration": 0
+        },
+    });
+
+    map.addControl(draw, 'top-right');
+    map.on('draw.update', drawUpdateOverlay.bind({
+        map: map
+    }));
+
+
+    draw.add(polygon);
+    // draw.changeMode('mode', opts);
+    // tx_rect, direct_select
+    draw.changeMode('tx_rect', {
+        featureId: polygon.id
+    });
+
 
 
 }
